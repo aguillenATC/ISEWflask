3e4b1877da81c8a2dc8e1bc144989444e743a200
Alberto Guillén <aguillen@ugr.es>
2021-01-29 19:37:26 +0100

    Código para la vista de register

--- isew/auth.py
+++ isew/auth.py
@@ -8,3 +8,33 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from isew.db import get_db 
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+
+def register():
+    if request.method == 'POST':
+        username = request.form['nombre']
+        password = request.form['upassword']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM usuario WHERE nombre = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO usuario (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            ) #TODO añdir pepper
+            db.commit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/register.html')
