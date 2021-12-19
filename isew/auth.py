9ec9102a39625112ea65e34b03ee745939399f45
Alberto Guillén <aguillen@ugr.es>
2021-12-19 23:34:13 +0100

    añadimos blueprint y vista para login

--- isew/auth.py
+++ isew/auth.py
@@ -13,8 +13,8 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 def register():
     if request.method == 'POST':
-        username = request.form['nombre']
-        password = request.form['upassword']
+        username = request.form['username']
+        password = request.form['password']
         db = get_db()
         error = None
 
@@ -29,12 +29,37 @@ def register():
 
         if error is None:
             db.execute(
-                'INSERT INTO usuario (username, password) VALUES (?, ?)',
+                'INSERT INTO usuario (nombre, upassword) VALUES (?, ?)',
                 (username, generate_password_hash(password))
             ) #TODO añdir pepper
             db.commit()
             return redirect(url_for('auth.login'))
 
-        flash(error)
+        flash(error) # flash() stores messages that can be retrieved when rendering the template.
 
     return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM usuario WHERE nombre = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Nombre usuario incorrecot.'
+        elif not check_password_hash(user['upassword'], password):
+            error = 'Contraseña incorrecta.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
